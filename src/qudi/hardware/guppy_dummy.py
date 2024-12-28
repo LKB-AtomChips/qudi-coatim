baadbb5078a9c86556f37c335178e6b85ea10241
ClementRaphin <clement.raphin@gmail.com>
2024-12-28 13:30:14 +0100

    - Created a camera_interface from iqo modules - Copied contents of iqo camera_dummy to guppy_dummy

--- src/qudi/hardware/guppy_dummy.py
+++ src/qudi/hardware/guppy_dummy.py
@@ -4,36 +4,146 @@ __all__ = ['GuppyDummy']
 
 import time
 
-from qudi.interface.absorption_interface import AbsorptionInterface
+from qudi.interface.camera_interface import CameraInterface
 from qudi.core.statusvariable import StatusVar
 from qudi.core.configoption import ConfigOption
 from qudi.util.mutex import Mutex
 
 
-class GuppyDummy(AbsorptionInterface):
-    """ Dummy module for AlliedVision GuppyPro camera """
+class GuppyDummy(CameraInterface):
+    """ Dummy module for AlliedVision GuppyPro camera
 
-    # Declare static parameters that can/must be declared in the qudi configuration
-    _trigger_time = ConfigOption(name='trigger_time', default=0.001, missing='warn')
+    Example config for copy-paste:
 
-    # Declare status variables that are saved in the AppStatus upon deactivation of the module and
-    # are initialized to the saved value again upon activation.
-    # _my_status_variable = StatusVar(name='my_status_variable', default=0)
+    camera_dummy:
+        module.Class: 'camera.camera_dummy.CameraDummy'
+        options:
+            support_live: True
+            camera_name: 'Dummy camera'
+            # FIXME: resolution config option causes no image
+            # resolution: (1280, 720)
+            exposure: 0.1
+            gain: 1.0
+    """
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._mutex = Mutex()
 
-    def on_activate(self) -> None:
+    _support_live = ConfigOption('support_live', True)
+    _camera_name = ConfigOption('camera_name', 'Dummy camera')
+    _resolution = ConfigOption('resolution', (1280, 720))  # High-definition !
+
+    _live = False
+    _acquiring = False
+    _exposure = ConfigOption('exposure', .1)
+    _gain = ConfigOption('gain', 1.)
+
+    def on_activate(self):
+        """ Initialisation performed during activation of the module.
+        """
         pass
 
-    def on_deactivate(self) -> None:
-        pass
+    def on_deactivate(self):
+        """ Deinitialisation performed during deactivation of the module.
+        """
+        self.stop_acquisition()
 
-    @property
-    def trigger_time(self) -> float:
-        return self._trigger_time
+    def get_name(self):
+        """ Retrieve an identifier of the camera that the GUI can print
 
-    def send_trigger(self) -> None:
-        with self._mutex:
-            time.sleep(self._trigger_time)
+        @return string: name for the camera
+        """
+        return self._camera_name
+
+    def get_size(self):
+        """ Retrieve size of the image in pixel
+
+        @return tuple: Size (width, height)
+        """
+        return self._resolution
+
+    def support_live_acquisition(self):
+        """ Return whether or not the camera can take care of live acquisition
+
+        @return bool: True if supported, False if not
+        """
+        return self._support_live
+
+    def start_live_acquisition(self):
+        """ Start a continuous acquisition
+
+        @return bool: Success ?
+        """
+        if self._support_live:
+            self._live = True
+            self._acquiring = False
+
+    def start_single_acquisition(self):
+        """ Start a single acquisition
+
+        @return bool: Success ?
+        """
+        if self._live:
+            return False
+        else:
+            self._acquiring = True
+            time.sleep(float(self._exposure+10/1000))
+            self._acquiring = False
+            return True
+
+    def stop_acquisition(self):
+        """ Stop/abort live or single acquisition
+
+        @return bool: Success ?
+        """
+        self._live = False
+        self._acquiring = False
+
+    def get_acquired_data(self):
+        """ Return an array of last acquired image.
+
+        @return numpy array: image data in format [[row],[row]...]
+
+        Each pixel might be a float, integer or sub pixels
+        """
+        data = np.random.random(self._resolution)*self._exposure*self._gain
+        return data.transpose()
+
+    def set_exposure(self, exposure):
+        """ Set the exposure time in seconds
+
+        @param float time: desired new exposure time
+
+        @return float: setted new exposure time
+        """
+        self._exposure = exposure
+        return self._exposure
+
+    def get_exposure(self):
+        """ Get the exposure time in seconds
+
+        @return float exposure time
+        """
+        return self._exposure
+
+    def set_gain(self, gain):
+        """ Set the gain
+
+        @param float gain: desired new gain
+
+        @return float: new exposure gain
+        """
+        self._gain = gain
+        return self._gain
+
+    def get_gain(self):
+        """ Get the gain
+
+        @return float: exposure gain
+        """
+        return self._gain
+
+    def get_ready_state(self):
+        """ Is the camera ready for an acquisition ?
+
+        @return bool: ready ?
+        """
+        return not (self._live or self._acquiring)
